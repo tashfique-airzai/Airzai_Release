5c5f99cd46fa59354919748996cd6fd30285c422
cyril.austin.macdonald <cyril.macdonald@icloud.com>
2019-09-23 18:34:23 -0400

    Adding template

--- dev/Build/Template/0/ota.cmd
+++ dev/Build/Template/0/ota.cmd
@@ -18,7 +18,7 @@
     {
         "digest":"<insert digest>",
         "certificate":"",
-        "signature_base64":"<insert signature>",
+        "signature_base64":"P+/6uq2EImQxIQoORmaHPGCa37vqMNOa0GUXRt7xMoNzUFAVdKEQUgp9gvl/eaADw3FszltHUI+PkOaeyQYsIwIV+HksCoqvuzorpNJcDuh4QMgmgyPxntKUDPX3qA3e0gBkH2EcaWKYTMc9DOQrCHfJrL39pY+JSOxiuK6DM9HM4huO/EGI9ioFk4UVV6j+YjbdjuO2E6OKPnoY3uQRPoUqLmaGE4xai6fe2Kq+cvsV/Vj2/H5j8sXp3xdSBupeGlnySUmcb1k7IwQMUVJadd8haEQKjNlm/fz2SFEaDQpHgpTt15/HjhjACc+kctT+97QOg7qzUIYLctIryDHnlw==",
         "secured":1,
         "bundle":1,
         "filename":"/sys/servicepack.ucf"
