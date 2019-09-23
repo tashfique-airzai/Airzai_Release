ebd8c95ea3f7851704dc48c87990560acef14a1d
cyril.austin.macdonald <cyril.macdonald@icloud.com>
2019-09-23 12:20:48 -0400

    Starting test

---
+++ build/template/0/ota.cmd
@@ -0,0 +1,26 @@
+[
+    {
+        "digest":"<insert digest>",
+        "certificate":"dummy-root-ca-cert",
+        "signature_base64":"<insert signature>",
+        "secured":1,
+        "bundle":1,
+        "filename":"/sys/mcuflashimg.bin"
+    },
+    {
+        "digest":"34941765501d16a4ab776c3a74d89945f1a2575c5893069f62ffbe803f344549",
+        "certificate":"",
+        "signature_base64":"",
+        "secured":0,
+        "bundle":0,
+        "filename":"dummy-root-ca-cert"
+    },
+    {
+        "digest":"<insert digest>",
+        "certificate":"",
+        "signature_base64":"<insert signature>",
+        "secured":1,
+        "bundle":1,
+        "filename":"/sys/servicepack.ucf"
+    }
+]
