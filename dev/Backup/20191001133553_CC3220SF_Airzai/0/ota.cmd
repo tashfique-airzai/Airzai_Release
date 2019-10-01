48606c6c199bca6ac5f36d69e3aa3676a5ab0bcd
cyrilairzai <cyrilm@airzai.com>
2019-10-01 13:58:25 -0400

    Adding OTA package 2.0.80

---
+++ dev/Backup/20191001133553_CC3220SF_Airzai/0/ota.cmd
@@ -0,0 +1,26 @@
+[
+    {
+        "digest":"8e811557e8fb3f25d780e2c0067ff74822c503421ed102cb79792d822e3e1538",
+        "certificate":"",
+        "signature_base64":"",
+        "secured":1,
+        "bundle":1,
+        "filename":"/sys/mcuflashimg.bin"
+    },
+    {
+        "digest":"34941765501d16a4ab776c3a74d89945f1a2575c5893069f62ffbe803f344549",
+        "certificate":"",
+        "signature_base64":"",
+        "secured":0,
+        "bundle":0,
+        "filename":"dummy-root-ca-cert"
+    },
+    {
+        "digest":"5685a25b1060d40e50cb194a78f55edc825abb6b16b45c87078ab8c6609de027",
+        "certificate":"",
+        "signature_base64":"P+/6uq2EImQxIQoORmaHPGCa37vqMNOa0GUXRt7xMoNzUFAVdKEQUgp9gvl/eaADw3FszltHUI+PkOaeyQYsIwIV+HksCoqvuzorpNJcDuh4QMgmgyPxntKUDPX3qA3e0gBkH2EcaWKYTMc9DOQrCHfJrL39pY+JSOxiuK6DM9HM4huO/EGI9ioFk4UVV6j+YjbdjuO2E6OKPnoY3uQRPoUqLmaGE4xai6fe2Kq+cvsV/Vj2/H5j8sXp3xdSBupeGlnySUmcb1k7IwQMUVJadd8haEQKjNlm/fz2SFEaDQpHgpTt15/HjhjACc+kctT+97QOg7qzUIYLctIryDHnlw==",
+        "secured":1,
+        "bundle":1,
+        "filename":"/sys/servicepack.ucf"
+    }
+]
